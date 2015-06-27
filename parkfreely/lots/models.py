cc5ef9353d93b524e0dd9d8b7b107552fd7c21a5
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-27 16:49:33 -0400

    actually saved merge conflict

--- parkfreely/lots/models.py
+++ parkfreely/lots/models.py
@@ -3,16 +3,21 @@ from geolocation.google_maps import GoogleMaps
 # Create your models here.
 
 class Lot(models.Model):
-     lon = models.FloatField()
+     lng = models.FloatField()
      lat = models.FloatField()
      safety = models.IntegerField(default=5)
      description = models.CharField(maxsize=200)
      biking = models.BooleanField()
      walking = models.BooleanField()
      publictransit = models.BooleanField()
+     rating = models.IntegerField(default=5)
+     upvote = IntegerField(default=0)
+     downvote = IntegerField(default=0)
+     #  the fields below are highly subject to change
+     car = BooleanField()
+     bike = BooleanField()
+     walk = BooleanField()
+
 
      def __str__(self):
           return 'Longitude: {}\nLattitude: {}\nRating: {}'.format(self.lon, self.lat, self.rating)
-
-class Votes():
-     pass
